437bfc3e7d4d86bd3603764433663398f4ca7039
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2025-05-22 08:38:14 +0800

    feat: enhance MainActivity layout with Surface and fillMaxSize modifier

--- app/src/main/java/io/github/arthurkun/generic/datastore/app/MainActivity.kt
+++ app/src/main/java/io/github/arthurkun/generic/datastore/app/MainActivity.kt
@@ -3,6 +3,10 @@ package io.github.arthurkun.generic.datastore.app
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 import io.github.arthurkun.generic.datastore.app.theme.GenericDataStoreAppTheme
 import io.github.arthurkun.generic.datastore.app.ui.MainScreen
 import io.github.arthurkun.generic.datastore.app.ui.MainViewModel
@@ -20,9 +24,14 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             GenericDataStoreAppTheme {
-                MainScreen(
-                    vm = mainViewModel
-                )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.surface
+                ) {
+                    MainScreen(
+                        vm = mainViewModel
+                    )
+                }
             }
         }
     }
