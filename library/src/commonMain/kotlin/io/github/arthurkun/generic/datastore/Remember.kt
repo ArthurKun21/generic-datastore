5f4ee05341d2cda667193bc5375722140bc143ad
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-05-22 13:23:48 +0800

    feat: add coroutine support for asynchronous preference updates in remember function

--- library/src/commonMain/kotlin/io/github/arthurkun/generic/datastore/Remember.kt
+++ library/src/commonMain/kotlin/io/github/arthurkun/generic/datastore/Remember.kt
@@ -4,7 +4,10 @@ package io.github.arthurkun.generic.datastore
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * Remembers the value of this preference and returns a [MutableState] that can be used to
@@ -16,12 +19,15 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 @Composable
 fun <T> Prefs<T>.remember(): MutableState<T> {
     val state = this.asFlow().collectAsStateWithLifecycle(defaultValue)
+    val scope = rememberCoroutineScope()
     return remember(this) {
         object : MutableState<T> {
             override var value: T
                 get() = state.value
                 set(value) {
-                    setValue(value)
+                    scope.launch(Dispatchers.IO) {
+                        set(value)
+                    }
                 }
 
             override fun component1(): T = value
