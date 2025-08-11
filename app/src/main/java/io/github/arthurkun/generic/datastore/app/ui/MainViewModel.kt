649606190bf2bba4abadfe2927a4cfbcfaa5d9aa
arthur <16458204+ArthurKun021@users.noreply.github.com>
2025-08-11 12:07:50 +0800

    fix: handle exceptions during preferences import and log errors

--- app/src/main/java/io/github/arthurkun/generic/datastore/app/ui/MainViewModel.kt
+++ app/src/main/java/io/github/arthurkun/generic/datastore/app/ui/MainViewModel.kt
@@ -1,5 +1,6 @@
 package io.github.arthurkun.generic.datastore.app.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.github.arthurkun.generic.datastore.app.domain.PreferenceStore
@@ -13,7 +14,11 @@ class MainViewModel(
     suspend fun exportPreferences() = preferenceStore.exportPreferences()
 
     fun importPreferences(data: String) = viewModelScope.launch {
-        val newData = data.toJsonMap()
-        preferenceStore.importPreferences(newData)
+        try {
+            val newData = data.toJsonMap()
+            preferenceStore.importPreferences(newData)
+        } catch (e: Exception) {
+            Log.e("MainViewModel", "Failed to import preferences", e)
+        }
     }
 }
