424fe4b1236d2479ca8447bfa2f893d80c2fe82e
arthur <16458204+ArthurKun021@users.noreply.github.com>
2026-02-07 20:40:06 +0800

    feat: add EnumSetPreference for storing sets of enum values with serialization

---
+++ generic-datastore/src/commonMain/kotlin/io/github/arthurkun/generic/datastore/preferences/EnumSetPreference.kt
@@ -0,0 +1,26 @@
+package io.github.arthurkun.generic.datastore.preferences
+
+import io.github.arthurkun.generic.datastore.core.Prefs
+
+/**
+ * Defines a preference for storing a [Set] of enum values using a string set preference key.
+ *
+ * Each enum value is serialized to its name for storage and deserialized back on retrieval.
+ * If deserialization of an individual element fails due to an unknown enum value, that element
+ * is skipped.
+ *
+ * @param T The enum type.
+ * @param key The key for the preference.
+ * @param defaultValue The default set of enum values to use if the key is not found
+ * (defaults to an empty set).
+ */
+@Suppress("unused")
+public inline fun <reified T : Enum<T>> PreferencesDatastore.enumSet(
+    key: String,
+    defaultValue: Set<T> = emptySet(),
+): Prefs<Set<T>> = serializedSet(
+    key = key,
+    defaultValue = defaultValue,
+    serializer = { it.name },
+    deserializer = { enumValueOf(it) },
+)
