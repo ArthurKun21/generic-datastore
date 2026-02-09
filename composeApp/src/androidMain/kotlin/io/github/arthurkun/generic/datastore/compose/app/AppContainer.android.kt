88aa2bee1d9f0228766cd1c5e9a9bc9012c22b72
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2026-02-09 08:50:03 +0800

    refactor: simplify datastore initialization in AppContainer for both Android and Desktop

--- composeApp/src/androidMain/kotlin/io/github/arthurkun/generic/datastore/compose/app/AppContainer.android.kt
+++ composeApp/src/androidMain/kotlin/io/github/arthurkun/generic/datastore/compose/app/AppContainer.android.kt
@@ -1,26 +1,13 @@
 package io.github.arthurkun.generic.datastore.compose.app
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.PreferenceDataStoreFactory
-import androidx.datastore.preferences.core.Preferences
 import io.github.arthurkun.generic.datastore.compose.app.domain.PreferenceStore
-import io.github.arthurkun.generic.datastore.preferences.GenericPreferencesDatastore
-import okio.Path.Companion.toPath
+import io.github.arthurkun.generic.datastore.preferences.createPreferencesDatastore
 
 actual class AppContainer(context: Context) {
 
-    private fun createDataStore(producePath: () -> String): DataStore<Preferences> =
-        PreferenceDataStoreFactory.createWithPath(
-            produceFile = { producePath().toPath() },
-        )
-
-    private fun createDataStore(context: Context): DataStore<Preferences> = createDataStore(
+    private val genericPreferenceDatastore = createPreferencesDatastore(
         producePath = { context.filesDir.resolve(preferenceName).absolutePath },
-    )
-
-    private val genericPreferenceDatastore = GenericPreferencesDatastore(
-        datastore = createDataStore(context),
     )
 
     actual val preferenceStore = PreferenceStore(
