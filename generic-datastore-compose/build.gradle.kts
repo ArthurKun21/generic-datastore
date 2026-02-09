47782f4d89d0d62fd7f899bbede7047ffa98012f
arthur <16458204+ArthurKun021@users.noreply.github.com>
2026-02-09 20:22:42 +0800

    feat: add experimental opt-in for Kotlin Native compiler options

--- generic-datastore-compose/build.gradle.kts
+++ generic-datastore-compose/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.vanniktech.maven.publish)
@@ -55,8 +57,13 @@ kotlin {
     compilerOptions {
         freeCompilerArgs.addAll(
             "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-            "-opt-in=kotlinx.cinterop.ExperimentalForeignApi",
         )
+    }
+}
+
+tasks.withType<KotlinNativeCompile>().configureEach {
+    compilerOptions {
+        optIn.add("kotlinx.cinterop.ExperimentalForeignApi")
     }
 }
 
