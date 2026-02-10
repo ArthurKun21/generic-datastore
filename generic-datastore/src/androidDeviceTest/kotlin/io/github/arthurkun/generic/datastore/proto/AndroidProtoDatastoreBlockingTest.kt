f23938e3f5e3aa840573515918742bf2d6c1a6a5
arthur <16458204+ArthurKun021@users.noreply.github.com>
2026-02-10 16:06:31 +0800

    refactor: move setup and teardown logic to instance methods

--- generic-datastore/src/androidDeviceTest/kotlin/io/github/arthurkun/generic/datastore/proto/AndroidProtoDatastoreBlockingTest.kt
+++ generic-datastore/src/androidDeviceTest/kotlin/io/github/arthurkun/generic/datastore/proto/AndroidProtoDatastoreBlockingTest.kt
@@ -10,58 +10,54 @@ import kotlinx.coroutines.cancel
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
-import org.junit.AfterClass
-import org.junit.BeforeClass
+import org.junit.After
+import org.junit.Before
 import org.junit.runner.RunWith
 import java.io.File
 
 @RunWith(AndroidJUnit4::class)
 class AndroidProtoDatastoreBlockingTest : AbstractProtoDatastoreBlockingTest() {
 
-    companion object {
-        private lateinit var _protoDatastore: GenericProtoDatastore<TestProtoData>
-        private lateinit var testScope: CoroutineScope
-        private lateinit var testContext: Context
+    private lateinit var _protoDatastore: GenericProtoDatastore<TestProtoData>
+    private lateinit var testScope: CoroutineScope
+    private lateinit var testContext: Context
 
-        @JvmStatic
-        @BeforeClass
-        fun setupClass() {
-            val dispatcher = UnconfinedTestDispatcher()
-            Dispatchers.setMain(dispatcher)
-            testContext = ApplicationProvider.getApplicationContext()
-            testScope = CoroutineScope(Job() + dispatcher)
-            _protoDatastore = createProtoDatastore(
-                serializer = TestProtoDataSerializer,
-                defaultValue = TestProtoData(),
-                scope = testScope,
-                producePath = {
-                    testContext.filesDir.resolve("datastore/test_proto_blocking.pb").absolutePath
-                },
-            )
-        }
+    override val protoDatastore: GenericProtoDatastore<TestProtoData> get() = _protoDatastore
 
-        @JvmStatic
-        @AfterClass
-        fun tearDownClass() {
+    @Before
+    fun setup() {
+        val dispatcher = UnconfinedTestDispatcher()
+        Dispatchers.setMain(dispatcher)
+        testContext = ApplicationProvider.getApplicationContext()
+        testScope = CoroutineScope(Job() + dispatcher)
+        _protoDatastore = createProtoDatastore(
+            serializer = TestProtoDataSerializer,
+            defaultValue = TestProtoData(),
+            scope = testScope,
+            producePath = {
+                testContext.filesDir.resolve("datastore/test_proto_blocking.pb").absolutePath
+            },
+        )
+    }
+
+    @After
+    fun tearDown() {
+        try {
+            if (::testScope.isInitialized) {
+                testScope.cancel()
+            }
+        } finally {
             try {
-                if (::testScope.isInitialized) {
-                    testScope.cancel()
+                if (::testContext.isInitialized) {
+                    val dataStoreFile =
+                        File(testContext.filesDir, "datastore/test_proto_blocking.pb")
+                    if (dataStoreFile.exists()) {
+                        dataStoreFile.delete()
+                    }
                 }
             } finally {
-                try {
-                    if (::testContext.isInitialized) {
-                        val dataStoreFile =
-                            File(testContext.filesDir, "datastore/test_proto_blocking.pb")
-                        if (dataStoreFile.exists()) {
-                            dataStoreFile.delete()
-                        }
-                    }
-                } finally {
-                    Dispatchers.resetMain()
-                }
+                Dispatchers.resetMain()
             }
         }
     }
-
-    override val protoDatastore: GenericProtoDatastore<TestProtoData> get() = _protoDatastore
 }
