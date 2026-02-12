b4d5426178ec3c34a7b06fd1c6d9f74cfe0572b5
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2026-02-12 13:09:00 +0800

    chore: apply spotless

--- generic-datastore/src/commonMain/kotlin/io/github/arthurkun/generic/datastore/proto/core/ProtoFieldPreference.kt
+++ generic-datastore/src/commonMain/kotlin/io/github/arthurkun/generic/datastore/proto/core/ProtoFieldPreference.kt
@@ -11,8 +11,8 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.runBlocking
