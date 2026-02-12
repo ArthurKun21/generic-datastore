d5a950d9e3166cf5344d4f9cf2733945f6212730
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2026-02-12 12:57:15 +0800

    tests: fix the imports for ios tests

--- generic-datastore/src/iosSimulatorArm64Test/kotlin/io/github/arthurkun/generic/datastore/proto/optional/custom/IosProtoNullableKserializedListFieldBlockingTest.kt
+++ generic-datastore/src/iosSimulatorArm64Test/kotlin/io/github/arthurkun/generic/datastore/proto/optional/custom/IosProtoNullableKserializedListFieldBlockingTest.kt
@@ -1,5 +1,6 @@
 package io.github.arthurkun.generic.datastore.proto.optional.custom
 
+import io.github.arthurkun.generic.datastore.proto.core.custom.IosCustomFieldProtoTestHelper
 import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 
