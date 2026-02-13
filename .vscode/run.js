352f014b6cb004736091d4a055c3c1b2ad6390bb
arthur <16458204+ArthurKun021@users.noreply.github.com>
2026-02-13 14:05:57 +0800

    fix: enable cross platform

--- .vscode/run.js
+++ .vscode/run.js
@@ -1,7 +1,9 @@
 const { execSync } = require("child_process");
+const cwd = require("path").resolve(__dirname, "..");
+const gradlew = process.platform === "win32" ? ".\\gradlew.bat" : "./gradlew";
 const args = process.argv.slice(2).join(" ");
 try {
-  execSync(args, { stdio: "inherit", cwd: __dirname + "/.." });
+  execSync(`${gradlew} ${args}`, { stdio: "inherit", cwd });
 } catch (e) {
   process.exit(e.status || 1);
 }
