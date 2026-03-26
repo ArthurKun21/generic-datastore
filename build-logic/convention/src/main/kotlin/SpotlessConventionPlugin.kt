51293029cdb316244e03349a81dc15a9608d5676
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2026-03-27 07:10:26 +0800

    chore(gradle): add build-logic

---
+++ build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
@@ -0,0 +1,37 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.getByType
+
+class SpotlessConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply("com.diffplug.spotless")
+
+            val libs = rootProject.extensions
+                .getByType<VersionCatalogsExtension>()
+                .named("libs")
+
+            extensions.configure<SpotlessExtension> {
+                kotlin {
+                    target("**/*.kt", "**/*.kts")
+                    targetExclude("**/build/**/*.kt")
+                    ktlint(
+                        libs.findLibrary("ktlint-core").get().get().version!!,
+                    ).editorConfigOverride(
+                        mapOf("ktlint_standard_annotation" to "disabled"),
+                    )
+                    trimTrailingWhitespace()
+                    endWithNewline()
+                }
+                format("xml") {
+                    target("**/*.xml")
+                    trimTrailingWhitespace()
+                    endWithNewline()
+                }
+            }
+        }
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,35 +6,16 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.spotless) apply false
     alias(libs.plugins.vanniktech.maven.publish) apply false
     alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.wire) apply false
+    id("generic-datastore.spotless") apply false
 }
 
-version = providers.environmentVariable("RELEASE_TAG")
-    .map { it.removePrefix("v") }
-    .getOrElse("1.0.0")
-
 subprojects {
-    version = rootProject.version
-    apply(plugin = "com.diffplug.spotless")
+    apply(plugin = "generic-datastore.spotless")
 
-    plugins.withId("com.vanniktech.maven.publish") {
-        configure<PublishingExtension> {
-            repositories {
-                maven {
-                    name = "GitHubPackages"
-                    url = uri("https://maven.pkg.github.com/ArthurKun21/generic-datastore")
-                    credentials {
-                        username = System.getenv("GITHUB_ACTOR")
-                        password = System.getenv("GITHUB_TOKEN")
-                    }
-                }
-            }
-        }
-    }
     tasks.withType<KotlinCompile>().configureEach {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_17)
@@ -44,21 +25,4 @@ subprojects {
             )
         }
     }
-    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
-        kotlin {
-            target("**/*.kt", "**/*.kts")
-            targetExclude("**/build/**/*.kt")
-            ktlint(libs.ktlint.core.get().version).editorConfigOverride(
-                mapOf("ktlint_standard_annotation" to "disabled"),
-            )
-            trimTrailingWhitespace()
-            endWithNewline()
-        }
-        format("xml") {
-            target("**/*.xml")
-            trimTrailingWhitespace()
-            endWithNewline()
-        }
-    }
 }
-
