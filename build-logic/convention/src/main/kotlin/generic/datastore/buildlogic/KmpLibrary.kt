bd018f0b18245be97436733abc9ebf425b73a256
arthur <16458204+ArthurKun021@users.noreply.github.com>
2026-03-27 15:05:39 +0800

    refactor: streamline KmpLibraryConventionPlugin configuration

--- build-logic/convention/src/main/kotlin/generic/datastore/buildlogic/KmpLibrary.kt
+++ build-logic/convention/src/main/kotlin/generic/datastore/buildlogic/KmpLibrary.kt
@@ -1,9 +1,7 @@
 package generic.datastore.buildlogic
 
-import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryTarget
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.Test
-import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
 
@@ -11,27 +9,8 @@ internal fun Project.configureKmpLibrary(
     kmpExtension: KotlinMultiplatformExtension,
 ) {
     kmpExtension.apply {
-        explicitApi()
 
-        applyDefaultHierarchyTemplate()
 
-        targets.withType<KotlinMultiplatformAndroidLibraryTarget>().configureEach {
-            compileSdk = AndroidConfig.COMPILE_SDK
-            minSdk = AndroidConfig.MIN_SDK
-        }
-
-        iosX64()
-        iosArm64()
-        iosSimulatorArm64()
-
-        jvm()
-
-        compilerOptions {
-            freeCompilerArgs.addAll(
-                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-                "-Xexpect-actual-classes",
-            )
-        }
     }
 
     tasks.withType(Test::class.java).configureEach {
