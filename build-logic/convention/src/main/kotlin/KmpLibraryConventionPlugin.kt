399531113358eea14c8cc710f068748dd559042a
arthur <16458204+ArthurKun021@users.noreply.github.com>
2026-03-27 15:05:39 +0800

    refactor: streamline KmpLibraryConventionPlugin configuration

--- build-logic/convention/src/main/kotlin/KmpLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/KmpLibraryConventionPlugin.kt
@@ -1,13 +1,45 @@
+import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryTarget
+import generic.datastore.buildlogic.AndroidConfig
 import generic.datastore.buildlogic.configureKmpLibrary
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
+@Suppress("unused")
 class KmpLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            pluginManager.apply("org.jetbrains.kotlin.multiplatform")
-            pluginManager.apply("com.android.kotlin.multiplatform.library")
+            with(pluginManager) {
+                apply("org.jetbrains.kotlin.multiplatform")
+                apply("com.android.kotlin.multiplatform.library")
+            }
+
+            extensions.configure<KotlinMultiplatformExtension> {
+                explicitApi()
+
+                applyDefaultHierarchyTemplate()
+
+                targets.withType<KotlinMultiplatformAndroidLibraryTarget>().configureEach {
+                    compileSdk = AndroidConfig.COMPILE_SDK
+                    minSdk = AndroidConfig.MIN_SDK
+                }
+
+                iosX64()
+                iosArm64()
+                iosSimulatorArm64()
+
+                jvm()
+
+                compilerOptions {
+                    freeCompilerArgs.addAll(
+                        "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                        "-Xexpect-actual-classes",
+                    )
+                }
+            }
+
 
             val kmpExtension =
                 extensions.getByType(KotlinMultiplatformExtension::class.java)
