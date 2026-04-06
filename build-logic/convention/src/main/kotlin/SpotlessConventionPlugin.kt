6470639db639873d8440cd0e521d154e030306cd
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2026-04-07 04:57:46 +0800

    refactor: suppress unused warning in SpotlessConventionPlugin

--- build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/SpotlessConventionPlugin.kt
@@ -5,6 +5,7 @@ import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
 
+@Suppress("unused")
 class SpotlessConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
