8220b9748836c0bb8484d7e7472c97a406892961
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2026-04-07 04:56:35 +0800

    refactor: enhance Android configuration setup in build logic

--- build-logic/convention/src/main/kotlin/generic/datastore/buildlogic/KmpLibrary.kt
+++ build-logic/convention/src/main/kotlin/generic/datastore/buildlogic/KmpLibrary.kt
@@ -1,17 +1,40 @@
 package generic.datastore.buildlogic
 
+import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.Test
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
+
+internal fun Project.configureAndroid(commonExtension: CommonExtension) {
+    commonExtension.apply {
+        compileSdk = AndroidConfig.COMPILE_SDK
+
+        defaultConfig.apply {
+            minSdk = AndroidConfig.MIN_SDK
+        }
+
+        compileOptions.apply {
+            sourceCompatibility = AndroidConfig.JavaVersion
+            targetCompatibility = AndroidConfig.JavaVersion
+        }
+    }
+}
+
+internal fun Project.configureCommonKotlinCompileOptions() {
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(AndroidConfig.JvmTarget)
+        }
+    }
+}
 
 internal fun Project.configureKmpLibrary(
     kmpExtension: KotlinMultiplatformExtension,
 ) {
-    kmpExtension.apply {
 
-
-    }
 
     tasks.withType(Test::class.java).configureEach {
         useJUnitPlatform()
