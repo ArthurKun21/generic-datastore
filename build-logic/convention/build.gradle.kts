402a7dc1051ffaac0030e59ef7dc1484135aec1b
Arthur <16458204+ArthurKun21@users.noreply.github.com>
2026-04-07 06:59:51 +0800

    chore(deps): add compose-gradle-plugin 1.10.3

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     compileOnly(libs.android.gradle.plugin)
     compileOnly(libs.kotlin.gradle.plugin)
     implementation(libs.spotless.gradle.plugin)
+    compileOnly(libs.compose.gradle.plugin)
     compileOnly(libs.vanniktech.maven.publish.plugin)
 }
 
